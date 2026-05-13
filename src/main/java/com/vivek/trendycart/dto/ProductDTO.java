8504628841c2cf2897bf836579f92f03c22d2e5d
Vivek Kuthey <vivekkuthey1@gmail.com>
2026-05-13 22:49:08 +0530

    Added welcome text

--- src/main/java/com/vivek/trendycart/dto/ProductDTO.java
+++ src/main/java/com/vivek/trendycart/dto/ProductDTO.java
@@ -1,9 +1,14 @@
 package com.vivek.trendycart.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
 public class ProductDTO {
 
+    @NotBlank(message = "Product name is required")
     private String name;
 
+    @Positive(message = "Price must be greater than 0")
     private double price;
 
     public String getName() {
